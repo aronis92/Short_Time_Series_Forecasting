f9d35f27329d1870e0014591245b376b5df102a1
Dimitris Aronis <dimitrisaronis@hotmail.com>
2020-11-21 19:30:36 +0200

    up

--- Code/AR.py
+++ Code/AR.py
@@ -15,10 +15,10 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 np.random.seed(0)
 
 #                             Index  Var x Time
-datasets = ['macro', #__________0     12 x 203    # 
+datasets = ['macro', #__________0     12 x 203    # DONE
             'elnino', #_________1     12 x 61     # DONE
             'ozone', #__________2      8 x 203    # DONE
-            'nightvisitors', #__3      8 x 56     #
+            'nightvisitors', #__3      8 x 56     # 
             'inflation', #______4      8 x 123    # DROP PROBABLY
             'nasdaq', #_________5     82 x 40560  # Pending
             'yahoo', #__________6     5 x 2469    #
@@ -29,7 +29,7 @@ datasets = ['macro', #__________0     12 x 203    #
 # Load the Dataset
 data_name = datasets[9]
 X_train, X_val, X_test = get_data(dataset = data_name,
-                                  Ns = [75, 1, 1])
+                                  Ns = [10, 1, 1])
 
 # Plot the loaded data
 # plt.figure(figsize = (12,5))
@@ -48,83 +48,39 @@ parameters = {'R1':2,
               'max_epoch': 15,
               'threshold': 0.000001}
 
+   
+start = time.clock()
+convergences, changes, A, prediction, Us = BHTAR(data_train = X_train,
+                                                 data_val = X_val,
+                                                 par = parameters,
+                                                 mod = "AR")
+end = time.clock()
+duration_AR = end - start
 
-file = open("results/BHT_AR_" + data_name + ".txt", 'a')
+rmse_AR = changes[:,0]
+nrmse_AR = changes[:,1]
 
-ds = [0, 1, 2, 3, 4, 5]
-l_list = [.1, .2, .3, .4, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
-min_v = 1000
-for p_val in range(1, 2):#,6):
-    parameters['p'] = p_val
-    
-    for r_val in range(2, 11):
-        parameters['r'] = r_val
-        
-        for d_val in ds:
-            parameters['d'] = d_val
-            
-            print("p:", p_val, "r:", r_val, "d:", d_val)
-            X_hat, _ = MDT(X_train, parameters['r'])
-            if parameters['d']>0:
-                X_hat, _ = difference(X_hat, parameters['d'])
-            Rs = get_ranks(X_hat)
-        
-            for R1_val in range(2, Rs[0]+1):
-                parameters['R1'] = R1_val
-                
-                for R2_val in range(2, r_val+1):
-                    parameters['R2'] = R2_val
-
-# for l in l_list:
-#     parameters['lam'] = l
-    
-                    start = time.clock()
-                    convergences, changes, A, prediction, Us = BHTAR(data_train = X_train,
-                                                                     data_val = X_val,
-                                                                     par = parameters,
-                                                                     mod = "AR")
-                    end = time.clock()
-                    duration_AR = end - start
+# Validation
+print("\nR1:", parameters['R1'], " R2:", parameters['R2'], " p:", parameters['p'], " r:", parameters['r'], " lam:", parameters['lam'])
+print("Validation RMSE_AR: ", rmse_AR[-1], min(rmse_AR))
+print("Validation NRMSE_AR: ", nrmse_AR[-1], min(nrmse_AR))
+print("Validation duration_AR: ", duration_AR)
                     
-                    rmse_AR = changes[:,0]
-                    nrmse_AR = changes[:,1]
-                    
-                    # Validation
-                    #print("\nR1:", parameters['R1'], " R2:", parameters['R2'], " p:", parameters['p'], " r:", parameters['r'])
-                    #print("\nlam:", parameters['lam'])
-                    #print("Validation RMSE_AR: ", rmse_AR[-1], min(rmse_AR))
-                    # print("Validation NRMSE_AR: ", nrmse_AR[-1], min(nrmse_AR))
-                    #print("Validation duration_AR: ", duration_AR)
-                    
-                    if nrmse_AR[-1] < min_v:
-                        min_v = nrmse_AR[-1]
-                        file.write("\nR1:"+str(R1_val)+"  R2:"+str(R2_val)+"  p:"+str(parameters['p'])+"  r:"+str(r_val)+"  d:"+str(d_val)+"  lam:" + str(parameters['lam'])) 
-                        #file.write("lambda:"+str(l))
-                        file.write("\nValidation RMSE_AR: "+str(rmse_AR[-1])) 
-                        file.write("\nValidation NRMSE_AR: "+str(nrmse_AR[-1])+"\n") 
-            
-
-file.close()
 
 
 
 ''' Check X_test_start'''
-# Prepare the data needed for the testing predictions
-# if X_val.shape[-1] >= parameters['p'] + parameters['r'] - 1:
-#     X_test_start = X_val 
-# else:
-#     X_test_start = np.append(X_train[..., -(parameters['p'] + parameters['r'] - 1 - X_val.shape[-1]):], X_val, axis=-1)
+X_test_start = np.append(X_train, X_val, axis=-1)
 
+test_rmse, test_nrmse = BHTAR_test(X_test_start,
+                                    X_test,
+                                    A, 
+                                    Us,
+                                    parameters, 
+                                    mod = "AR")
 
-# test_rmse, test_nrmse = BHTAR_test(X_test_start,
-#                                    X_test,
-#                                    A, 
-#                                    Us,
-#                                    parameters, 
-#                                    mod = "AR")
-
-#print("Test RMSE_VAR: ", test_rmse)
-# print("Test NRMSE_AR: ", test_nrmse)
+print("Test RMSE_AR: ", test_rmse)
+print("Test NRMSE_AR: ", test_nrmse)
 
 
 
@@ -132,21 +88,3 @@ file.close()
 # plot_results(convergences, 'BHT_AR Convergence', "Convergence Value")
 # plot_results(changes[:,0], 'BHT_AR RMSE', "RMSE Value")
 # plot_results(changes[:,1], 'BHT_AR ΝRMSE', "NRMSE Value")
-
-
-
-
-
-
-# Test
-# rmse_AR, nrmse_AR = predict("AR", Us, A, parameters, X, X_test)
-
-# print("Test RMSE_AR: ", rmse_AR)
-# print("Test NRMSE_AR: ", nrmse_AR)
-
-
-
-
-
-
-
--- Code/VAR.py
+++ Code/VAR.py
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 np.random.seed(0)
 
 #                             Index  Var x Time
-datasets = ['macro', #__________0     12 x 203    # 
+datasets = ['macro', #__________0     12 x 203    # DONE
             'elnino', #_________1     12 x 61     # DONE
             'ozone', #__________2      8 x 203    # DONE
             'nightvisitors', #__3      8 x 56     #
@@ -31,13 +31,6 @@ data_name = datasets[9]
 X_train, X_val, X_test = get_data(dataset = data_name,
                                   Ns = [75, 1, 1])
 
-# Plot the loaded data
-# plt.figure(figsize = (12,5))
-# #plt.ylim(20, 70)
-# plt.plot(X_train.T)
-# plt.show()
-
-
 # Set the algorithm's parameters
 parameters = {'R1':2,
               'R2':2,
@@ -49,82 +42,38 @@ parameters = {'R1':2,
               'threshold': 0.000001}
 
 
-file = open("results/BHT_VAR_" + data_name + ".txt", 'a')
+start = time.clock()
+convergences, changes, A, prediction, Us = BHTAR(data_train = X_train,
+                                                 data_val = X_val,
+                                                 par = parameters,
+                                                 mod = "VAR")
+end = time.clock()
+duration_VAR = end - start
 
-ds = [0, 1, 2, 3, 4, 5]
-l_list = [.1, .2, .3, .4, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 10, 50, 100, 1000]
-min_v = 1000
-for p_val in range(1, 2):#,6):
-    parameters['p'] = p_val
-    
-    for r_val in range(2, 11):
-        parameters['r'] = r_val
-        
-        for d_val in ds:
-            parameters['d'] = d_val
-            print("p:", p_val, "r:", r_val, "d:", d_val)
-            X_hat, _ = MDT(X_train, parameters['r'])
-            if parameters['d']>0:
-                X_hat, _ = difference(X_hat, parameters['d'])
-            Rs = get_ranks(X_hat)
+rmse_VAR = changes[:,0]
+nrmse_VAR = changes[:,1]
+
+# Validation
+print("\nR1:", parameters['R1'], " R2:", parameters['R2'], " p:", parameters['p'], " r:", parameters['r'], " lam:", parameters['lam'])
+print("Validation RMSE_VAR: ", rmse_VAR[-1], min(rmse_VAR))
+print("Validation NRMSE_VAR: ", nrmse_VAR[-1], min(nrmse_VAR))
+print("Validation duration_VAR: ", duration_VAR)
+
             
-            for R1_val in range(2, Rs[0]+1):
-                parameters['R1'] = R1_val
-                
-                for R2_val in range(2, r_val+1):
-                    parameters['R2'] = R2_val
-
-# for l in l_list:
-#     parameters['lam'] = l
-        
-                    start = time.clock()
-                    convergences, changes, A, prediction, Us = BHTAR(data_train = X_train,
-                                                                     data_val = X_val,
-                                                                     par = parameters,
-                                                                     mod = "VAR")
-                    end = time.clock()
-                    duration_VAR = end - start
-                    
-                    rmse_VAR = changes[:,0]
-                    nrmse_VAR = changes[:,1]
-                    
-                    # Validation
-                    #print("\nR1:", parameters['R1'], " R2:", parameters['R2'], " p:", parameters['p'], " r:", parameters['r'])
-                    #print("\nlam:", parameters['lam'])
-                    # print("Validation RMSE_VAR: ", rmse_VAR[-1], min(rmse_VAR))
-                    # print("Validation NRMSE_VAR: ", nrmse_VAR[-1], min(nrmse_VAR))
-                    #print("Validation duration_VAR: ", duration_VAR)
-                    
-                    if nrmse_VAR[-1] < min_v:
-                        min_v = nrmse_VAR[-1]
-                        #file.write("\nlambda: "+str(l))
-                        file.write("\nR1:"+str(R1_val)+"  R2:"+str(R2_val)+"  p:"+str(parameters['p'])+"  r:"+str(r_val)+"  d:"+str(d_val)+"  lam:" + str(parameters['lam'])) 
-                        file.write("\nValidation RMSE_VAR: "+str(rmse_VAR[-1])) 
-                        file.write("\nValidation NRMSE_VAR: "+str(nrmse_VAR[-1])+"\n")
-            
-
-file.close()
-
 
 
 ''' Check X_test_start'''
-# Prepare the data needed for the testing predictions
-# if X_val.shape[-1] >= parameters['p'] + parameters['r'] - 1:
-#     X_test_start = X_val 
-# else:
-#     X_test_start = np.append(X_train[..., -(parameters['p'] + parameters['r'] - 1 - X_val.shape[-1]):], X_val, axis=-1)
-
 X_test_start = np.append(X_train, X_val, axis=-1)
-X_test_start = X_test_start[..., -(parameters['p'] + parameters['d']):]
-# test_rmse, test_nrmse = BHTAR_test(X_test_start,
-#                                    X_test,
-#                                    A, 
-#                                    Us,
-#                                    parameters, 
-#                                    mod = "VAR")
 
-#print("Test RMSE_VAR: ", test_rmse)
-# print("Test NRMSE_VAR: ", test_nrmse)
+test_rmse, test_nrmse = BHTAR_test(X_test_start,
+                                   X_test,
+                                   A, 
+                                   Us,
+                                   parameters, 
+                                   mod = "VAR")
+
+print("Test RMSE_VAR: ", test_rmse)
+print("Test NRMSE_VAR: ", test_nrmse)
 
 
 
@@ -132,57 +81,3 @@ X_test_start = X_test_start[..., -(parameters['p'] + parameters['d']):]
 #plot_results(convergences, 'BHT_VAR Convergence', "Convergence Value")
 #plot_results(changes[:,0], 'BHT_VAR RMSE', "RMSE Value")
 #plot_results(changes[:,1], 'BHT_VAR ΝRMSE', "NRMSE Value")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Test
-# rmse_VAR, nrmse_VAR = predict("VAR", Us, A, parameters, X, X_test)
-
-# print("Test RMSE_AR: ", rmse_VAR)
-# print("Test NRMSE_AR: ", nrmse_VAR)
-
-
-# kron_T = np.kron(Us[1].T, Us[0].T)
-# kron_2 = np.kron(Us[1], Us[0])
-# A1_restored = np.linalg.multi_dot([np.linalg.pinv(kron_T), A[1], np.linalg.pinv(kron_2)])
-# A2_restored = np.linalg.multi_dot([np.linalg.pinv(kron_T), A[2], np.linalg.pinv(kron_2)])
-
-# from numpy import linalg as la
-# import matplotlib.pyplot as plt
-
-# np.random.seed(69)
-# total = 2000
-# X_total = np.zeros((15, total))
-# X_total[..., 0:2] = np.random.rand(15, 2)
-# for i in range(2, total):
-#     X_total[..., i] = np.dot(A1, X_total[..., i-1]) + np.dot(A2, X_total[..., i-2]) + np.random.rand(15,)
-# X = X_total[..., (total-50):]
-# plt.figure(figsize = (12,5))
-# X_vectorized = X[:5, :]
-# plt.plot(X_vectorized.T)
-# plt.show()
-
-
-# X_total = np.zeros((9, total))
-# X_total[..., 0:2] = np.random.rand(9, 2)
-# for i in range(2, total):
-#     X_total[..., i] = np.dot(A[0], X_total[..., i-1]) + np.dot(A[1], X_total[..., i-2]) + np.random.rand(9,)
-# X = X_total[..., (total-50):]
-# plt.figure(figsize = (12,5))
-# X_vectorized = X[:3, :]
-# plt.plot(X_vectorized.T)
-# plt.show()
-
-
-
